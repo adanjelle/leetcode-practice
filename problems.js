a3a9ce2a1b5ba9f103abe66b0126756ca2a2e7f7
adanjelle <adanjelle4@gmail.com>
2026-02-22 09:24:05 +0300

    made changes

--- problems.js
+++ problems.js
@@ -14,4 +14,24 @@ let isPalindrome= function(x){
   }
   return original === reversed;
 };
+// Palindrom quiz
+let longestPalindrome = function(s) {
+  let res = "";
 
+  const expand = (l, r) => {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++;
+    }
+    return s.slice(l + 1, r);
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let p1 = expand(i, i);
+    let p2 = expand(i, i + 1);
+    if (p1.length > res.length) res = p1;
+    if (p2.length > res.length) res = p2;
+  }
+
+  return res;
+};
